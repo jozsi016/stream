ea129bc30e5453e983f0c688027ebdda0fb681c4
Jozsef Nagy <jozsefnagy141@gmail.com>
2020-07-03 15:43:03 +0200

    Add system out and delete some commented code.

--- src/com/nagy/Main.java
+++ src/com/nagy/Main.java
@@ -1,8 +1,8 @@
 package com.nagy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
@@ -17,17 +17,79 @@ public class Main {
                 "I26", "I17", "I29",
                 "071");
 
-        List<String> gNumbers = new ArrayList<>();
-        gNumbers.add("str");
-        someBingNumbers
+//        List<String> gNumbers = new ArrayList<>();
+//        gNumbers.add("str");
+//        someBingNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .forEach(System.out::println);
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "071");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17","I29", "071");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+//        System.out.println("-------------------------------");
+//        System.out.println(concatStream
+//                .distinct()
+//                .peek(System.out::println)
+//                .count());
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List <Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+      Scanner input = new Scanner(System.in);
+
+        System.out.println("-------------------");
+//        List<String> sortedGNumbers = someBingNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s-> s.startsWith("G") || s.startsWith("N"))
+//                .sorted()
+//                .collect(Collectors.toList());
+
+        List<String> sortedGNumbers = someBingNumbers
                 .stream()
                 .map(String::toUpperCase)
-                .filter(s -> s.startsWith("G"))
+                .filter(s-> s.startsWith("G") || s.startsWith("N"))
                 .sorted()
-                .forEach(System.out::println);
-        someBingNumbers.stream().sorted().sorted() ;
+                .collect(ArrayList::new, ArrayList::add,  ArrayList::addAll);
 
-        System.out.println("Some thing to push!");
+        for(String s : sortedGNumbers) {
+            System.out.println(s);
+        }
 
-    }
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+       List<String> list =  Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+            .filter(s -> s.length() == 3).collect(Collectors.toList());
+
+       list.forEach(System.out::println);
+
+     }
 }
